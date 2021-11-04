4a8f954e921a20c93429df49c4731f66b1bc6db0
Robert Finkley <me@robertfinkley.com>
2021-11-04 15:46:50 -0400

    added morgan and body-parser middleware

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 
 //import routes
 const authRoutes = require("./routes/auth");
 
-//middleware
+//app middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+// Test if express.json will work in place of bodyParser
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.use(cors()); // enable same-origin resource sharing
+
+if (process.env.NODE_ENV == "development") {
+  // if in development allow localhost:3000
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
+
+//route middleware
 app.use("/api", authRoutes);
 
 const port = process.env.port || 8000;

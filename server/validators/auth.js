0288470c06080c8f52d62a5085138adc22f8f975
Robert Finkley <me@robertfinkley.com>
2021-11-05 16:55:01 -0400

    Added express-validator for signin

--- server/validators/auth.js
+++ server/validators/auth.js
@@ -7,3 +7,10 @@ exports.userSignUpValidator = [
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters long"),
 ];
+
+exports.userSigninValidator = [
+  check("email").isEmail().withMessage("Must be a valid email address"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+];

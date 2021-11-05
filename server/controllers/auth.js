f4d8201c9d87bcdb199d7d84493285dff90bc3a1
Robert Finkley <me@robertfinkley.com>
2021-11-05 16:50:34 -0400

    added accountActivation controller logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const sgMail = require("@sendgrid/mail");
+const user = require("../models/user");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // ****EXAMPLE - Simple User Signup****
@@ -64,4 +65,39 @@ exports.signup = (req, res) => {
         });
       });
   });
+};
+
+exports.accountActivation = (req, res) => {
+  const { token } = req.body;
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      function (err, decoded) {
+        if (err) {
+          console.log("JWT VERIFY IN ACCOUNT ACTIVATION ERROR", err);
+          return res.status(401).json({
+            error: "Expired link. Signup again",
+          });
+        }
+        const { name, email, password } = jwt.decode(token);
+        const user = new User({ name, email, password });
+        user.save((err, user) => {
+          if (err) {
+            console.log("SAVE USER IN ACCT ACTIVATION ERROR", err);
+            return res.status(401).json({
+              error: "Error saving use in database. Try signup again.",
+            });
+          }
+          return res.json({
+            message: "Signup success. Please signin.",
+          });
+        });
+      }
+    );
+  } else {
+    return res.json({
+      message: "Something went wrong. Try again.",
+    });
+  }
 };

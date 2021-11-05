77fa29b19a1ad09471eece1a6c1b0277d02c940e
Robert Finkley <me@robertfinkley.com>
2021-11-05 16:58:02 -0400

    added signin route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,13 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 //import controllers
-const { signup, accountActivation } = require("../controllers/auth");
+const { signin, signup, accountActivation } = require("../controllers/auth");
 
 //import validators
-const { userSignUpValidator } = require("../validators/auth");
+const {
+  userSignUpValidator,
+  userSigninValidator,
+} = require("../validators/auth");
 const { runValidation } = require("../validators/");
 
 router.post("/signup", userSignUpValidator, runValidation, signup);
+router.post("/signin", userSigninValidator, runValidation, signin);
 router.post("/account-activation", accountActivation);
 
 module.exports = router;

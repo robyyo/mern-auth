6dde348fdbd08bc828cdc43d0c6634d6109dfa63
Robert Finkley <me@robertfinkley.com>
2021-11-05 16:18:10 -0400

    adding new route for account activation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 //import controllers
-const { signup } = require("../controllers/auth");
+const { signup, accountActivation } = require("../controllers/auth");
 
 //import validators
 const { userSignUpValidator } = require("../validators/auth");
 const { runValidation } = require("../validators/");
 
 router.post("/signup", userSignUpValidator, runValidation, signup);
+router.post("/account-activation", accountActivation);
 
 module.exports = router;

29e459fdbb7ca10f0d9a009f169d95f967447f0b
Robert Finkley <me@robertfinkley.com>
2021-11-12 20:24:15 -0500

    moved code for readabillity

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { requireSignin } = require("../controllers/auth");
 const router = express.Router();
 
 //import controllers
 const { read } = require("../controllers/user");
+const { requireSignin } = require("../controllers/auth");
 
 router.get("/user/:id", requireSignin, read);
 

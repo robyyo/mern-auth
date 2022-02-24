d8e5a1803583aa39679f3e11d1f78b3487be2884
Robert Finkley <me@robertfinkley.com>
2022-02-23 19:03:10 -0500

    implementing forgot & reset password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -150,3 +150,68 @@ exports.adminMiddleware = (req, res, next) => {
     next();
   });
 };
+
+exports.forgotPassword = (req, res) => {
+  const { email } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User with that email does not exist",
+      });
+    }
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_RESET_PASSWORD, {
+      expiresIn: "10m",
+    });
+    const emailData = {
+      from: process.env.EMAIL_FROM,
+      to: email,
+      subject: `Password Reset link`,
+      html: `<p>Please use the following link to reset your password</p><p>${process.env.CLIENT_URL}/auth/password/reset/${token}</p><hr /><p>This email may contain sensitive information.</p><p>${process.env.CLIENT_URL}</p>`,
+    };
+    return user.updateOne({ resetPasswordLink: token }, (err, success) => {
+      if (err) {
+        console.log("RESET PASSWORD LINK ERROR", err);
+        return res.status(400).json({
+          error: "Database connection error on user forgot password request",
+        });
+      } else {
+        sgMail
+          .send(emailData)
+          .then((sent) => {
+            // console.log("SIGNUP EMAIL SENT:", sent);
+            return res.json({
+              message: `Email has been sent to ${email}. Follow the instructions to reset your password.`,
+            });
+          })
+          .catch((err) => {
+            // console.log("SIGNUP EMAIL SENT ERROR", err);
+            return res.json({
+              message: err.message,
+            });
+          });
+      }
+    });
+  });
+};
+
+exports.resetPassword = (req, res) => {
+  const { resetPasswordLink, newPassword } = req.body;
+  if (resetPasswordLink) {
+    jwt.verify(
+      resetPasswordLink,
+      process.env.JWT_RESET_PASSWORD,
+      (err, decoded) => {
+        if (err) {
+          return res.status(400).json({ error: "Expired link. Try again" });
+        }
+        User.findOne({resetPasswordLink}, (err, user) => {
+          if (err || !user) {
+            return res.status(400).json({
+              error: 'Something went wrong. Try later'
+            });
+          } 
+        })
+      }
+    );
+  }
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,17 +2,37 @@ const express = require("express");
 const router = express.Router();
 
 //import controllers
-const { signin, signup, accountActivation } = require("../controllers/auth");
+const {
+  signin,
+  signup,
+  accountActivation,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/auth");
 
 //import validators
 const {
   userSignUpValidator,
   userSigninValidator,
+  forgotPasswordValidator,
+  resetPasswordValidator,
 } = require("../validators/auth");
 const { runValidation } = require("../validators/");
 
 router.post("/signup", userSignUpValidator, runValidation, signup);
 router.post("/signin", userSigninValidator, runValidation, signin);
 router.post("/account-activation", accountActivation);
+router.put(
+  "/forgot-password",
+  forgotPasswordValidator,
+  runValidation,
+  forgotPassword
+);
+router.put(
+  "/reset-password",
+  resetPasswordValidator,
+  runValidation,
+  resetPassword
+);
 
 module.exports = router;
